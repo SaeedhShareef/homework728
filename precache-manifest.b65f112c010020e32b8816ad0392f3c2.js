8609da9a2f96b6be28be2eb9a80702ad4bc76d6d
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-30 09:55:47 -0400

    Updates

---
+++ precache-manifest.b65f112c010020e32b8816ad0392f3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6343d189fc0162d215af8b970071ea27",
+    "url": "/homework728/index.html"
+  },
+  {
+    "revision": "6797ff4a410144e6c9f1",
+    "url": "/homework728/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1fd80b3babe244ccf3eb",
+    "url": "/homework728/static/js/2.cd84425f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework728/static/js/2.cd84425f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6797ff4a410144e6c9f1",
+    "url": "/homework728/static/js/main.30dc2d2e.chunk.js"
+  },
+  {
+    "revision": "98a6aa62dc0a8e5440d9",
+    "url": "/homework728/static/js/runtime-main.3c42a868.js"
+  }
+]);
