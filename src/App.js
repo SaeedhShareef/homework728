ecd7ac61768f2cf3204e16fc04e7968528b31b41
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-30 01:38:48 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import Ninjas from "./Ninjas"
+import React, { Component } from 'react';
+import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1},
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2},
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3}
+    ]
+  }
+  addNinja = (ninja) =>{
+      ninja.id = Math.random();
+      let ninjas = [...this.state.ninjas, ninja];
+      this.setState({
+        ninjas: ninjas
+      })
+  }
+  render() {
+      return (
+        <div className="App">
         <h1>My first React App</h1>
-        <p>Welcome :D :O :P</p>
-        <Ninjas name="Ryu" age="25" belt = "Black"/>
-        <Ninjas name="Yoshi" age="30" belt = "Green"/>
-    </div>
-  );
-}
+         <p>Welcome :D :O :P</p>
+            <Ninjas ninjas={this.state.ninjas}/>
+            <AddNinja addNinja={this.addNinja}/>
+        </div>
+      );
+  }
+}  
 
-export default App;
+export default App
+
+
+
+
+
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class Ninjas extends Component{
-    render() {
-        // console.log(this.props)
-        const {name, age, belt} = this.props
-        return (
-            <div className="ninja">
-                    <div>Name: {name}</div>
-                    <div>Age: {age}</div>
-                    <div>Belt: {belt}</div>
-                            </div>
-        )
-    }
+const Ninjas = ({ninjas}) => {
+           const ninjaList = ninjas.map(ninja => {
+                   if(ninja.age> 20){
+            return (
+               <div className="ninja" key={ninja.id}>
+                 <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+              <div>Belt: {ninja.belt}</div>
+                         </div>
+             )
+         } else {
+             return null
+         }
+         })
+            
+           return(
+               <div className="ninja-list">
+                   {ninjaList}
+                   </div>
+           ) 
+        
+    
 
 }
 
