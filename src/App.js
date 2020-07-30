5c92e393138009288fb205a3fc9652799b86039d
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-30 10:07:01 -0400

    kaijadidit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3}
     ]
   }
-  addNinja = (ninja) =>{
-      ninja.id = Math.random();
-      let ninjas = [...this.state.ninjas, ninja];
-      this.setState({
-        ninjas: ninjas
-      })
-  }
+  
   render() {
       return (
         <div className="App">
